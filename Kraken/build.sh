985e052cbc62b40c277318c6a47a011e75ee2238
geggo <geggo@localhost>
2011-07-22 23:05:07 +0000

    updated build script

--- Kraken/build.sh
+++ Kraken/build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-cd ../a5_cpu/;./build.sh;cd ../Kraken/;cp ../a5_cpu/A5Cpu.so .
+echo Building A5Cpu.so...
+(
+cd ../a5_cpu/;
+./build.sh;
+)
+cp ../a5_cpu/A5Cpu.so .
+echo Building A5Gpu.so...
+(
+cd ../a5_ati/;
+make
+)
+cp ../a5_cpu/A5Gpu.so .
 
-g++ -O2 -DEMBED_FINDKC -o kraken Kraken.cpp NcqDevice.cpp DeltaLookup.cpp Fragment.cpp ServerCore.cpp ../a5_cpu/A5CpuStubs.cpp ../a5_ati/A5AtiStubs.cpp ../A5Util/Bidirectional.cpp ../A5Util/TheMatrix.cpp ../A5Util/find_kc.cpp -I. -lpthread -ldl -D__STDC_LIMIT_MACROS
+echo Building Kraken...
+
+g++ -O2 -DEMBED_FINDKC -o kraken Kraken.cpp NcqDevice.cpp DeltaLookup.cpp Fragment.cpp ServerCore.cpp ../a5_cpu/A5CpuStubs.cpp ../a5_ati/A5GpuStubs.cpp ../A5Util/Bidirectional.cpp ../A5Util/TheMatrix.cpp ../A5Util/find_kc.cpp -I. -lpthread -ldl -D__STDC_LIMIT_MACROS
